a6a1d693027862a62b8287d854a6121db444c9b6
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2024-12-09 21:22:03 +0900

    feat: Modified cleaning process to output files with anomalies removed

---
+++ research/main/rri_data_cutout.py
@@ -0,0 +1,20 @@
+# %%
+import pandas as pd
+from icecream import ic
+
+file_path = "../../../data/プロアシスト脳波・心拍_copy/2018年度（男性・自宅・避難所・車中泊）/心拍/DA_sheet.csv"
+
+# ファイルを、最初の5行をスキップして読み込む
+data_2 = pd.read_csv(file_path, encoding="shift-jis", skiprows=5)
+
+# 必要な行をフィルタリング（1290から41820まで、30行ごとに取得）
+cutout_data = data_2.iloc[1290:41850:30]
+
+ic(cutout_data)
+
+output_file = "../../../data/プロアシスト脳波・心拍_copy/2018年度（男性・自宅・避難所・車中泊）/心拍/DA_sheet_cutout.csv"
+
+# CSVファイルへの書き込み
+cutout_data.to_csv(
+    output_file, index=False, encoding="shift-jis"
+)  # 日本語に対応するエンコーディング
